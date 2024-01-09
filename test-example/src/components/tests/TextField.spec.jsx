e44cee8282cdafe0edc265871f90c212d35d3589
choiseona <tjsdk617@naver.com>
2024-01-09 22:47:59 +0900

    feat: className prop으로 설정한 CSS 클래스가 적용된다
    className props으로 설정한 CSS 클래스가 적용된다를 테스트를 통해 검증하는 코드

--- test-example/src/components/tests/TextField.spec.jsx
+++ test-example/src/components/tests/TextField.spec.jsx
@@ -4,6 +4,9 @@ import React from 'react';
 import TextField from '@/components/TextField';
 import render from '@/utils/test/render';
 
-it('sample test.', async () => {
-  expect(1).toBe(1);
+it('className prop으로 설정한 CSS 클래스가 적용된다.', async () => {
+  await render(<TextField className="my-class" />);
+  expect(screen.getByPlaceholderText('텍스트를 입력해 주세요.')).toHaveClass(
+    'my-class',
+  );
 });
